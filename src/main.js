6d32fc41057af1f15b002ede8134702b478178fb
Venipa <admin@venipa.net>
2021-03-30 21:58:14 +0200

    updated userjs href filter

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import tippy from "tippy.js";
       delete el.dataset.toggle;
       const title = el.dataset.originalTitle || el.title;
       if (!title) return;
+      el.dataset.tippyEnabled = true;
       tippy(el, {
         theme: "discord",
         content: title,
@@ -24,8 +25,9 @@ import tippy from "tippy.js";
     }
   );
   document.querySelectorAll("a").forEach(el => {
-    if (el.href && !el.href.startsWith(location.origin) && !el.href.startsWith('/')) {
+    if (el.href && !el.href.startsWith(location.origin) && !el.href.startsWith('/') && el.href.startsWith('http') && !el.dataset.tippyEnabled) {
       const url = new URL(el.href);
+      el.dataset.tippyEnabled = true;
       tippy(el, {
         theme: "discord",
         content: `Visit ${url.hostname}${url.pathname === "/" ? '' : url.pathname}${url.search || ''}`,
@@ -33,7 +35,7 @@ import tippy from "tippy.js";
         inertia: true,
         animation: "discord-anim",
         duration: [100, 100],
-        hideOnClick: false,
+        hideOnClick: false
       });
     }
   })
